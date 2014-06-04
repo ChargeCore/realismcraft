6f5f6afb7173036451f6c1f7a62efccbb220d291
EdBrown <chargecore@live.co.uk>
2014-06-04 17:39:49 +0100

    Added stuff to vanilla tweaks

--- ForgeMods/src/main/java/com/realismcraft/mainclasses/RCEventHandler.java
+++ ForgeMods/src/main/java/com/realismcraft/mainclasses/RCEventHandler.java
@@ -1,14 +1,25 @@
 package com.realismcraft.mainclasses;
 
+import java.util.Random;
+
+import scala.Console;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 
 public class RCEventHandler {
 
+	
+	public static boolean getStickChance(){
+		Random x = new Random();
+		boolean chance = x.nextBoolean();
+		return chance;
+	}
+	
 	@SubscribeEvent
 	public void onBlockHarvest(HarvestDropsEvent event) {
 	
@@ -16,16 +27,42 @@ public class RCEventHandler {
 				event.drops.clear();
 				event.drops.add(new ItemStack(Items.gold_nugget, 8));
 	
+			}	
+			
+			
+			
+			if (event.block == Blocks.leaves) {
+				getStickChance();
+				if(getStickChance()){
+				event.drops.clear();
+				event.drops.add(new ItemStack(Items.stick, 1));
+				}
+	
+			}	
+	}
+	
+
+	
+	@SubscribeEvent
+	public void BlockBreakTweeks(BreakSpeed event)
+	{
+		if (event.block == Blocks.log)  {
+			try
+			{
+				String toolClass = event.block.getHarvestTool(0);
+				if(event.block.getHarvestLevel(0) <= event.entityPlayer.inventory.getCurrentItem().getItem().getHarvestLevel(event.entityPlayer.inventory.getCurrentItem(), toolClass)){
+					event.newSpeed = event.originalSpeed;
+				}
+				else
+				{
+					event.newSpeed = -1;
+					Console.println("Didn't break block. Block Harvest Level: " + event.block.getHarvestLevel(0) + "   Tool Harvest Level: " + event.entityPlayer.inventory.getCurrentItem().getItem().getHarvestLevel(event.entityPlayer.inventory.getCurrentItem(), toolClass));
+				}
+			}
+			catch(NullPointerException ex){
+				event.newSpeed = -1;
+			}
 		}
 	}
+	
 }
-	
-
-	
-	//@SubscribeEvent
-	//public void onBreakSpeed(BreakSpeed event){
-		//event.newSpeed = -1;
-	
-	
-//}
-
